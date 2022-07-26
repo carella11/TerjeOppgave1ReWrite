520ed37ff12138aa08ca73efc4ce3f0c83cc9eb7
carella11 <christianar@getacademy.no>
2022-07-26 12:45:59 +0200

    Add project files.

---
+++ TerjeOppgave1ReWrite/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TerjeOppgave1ReWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalDistance = 0.0;
+            var id = 1;
+            string filePath = "C:\\Users\\GET Academy\\Desktop\\Modul 3 Prosjekter\\TerjeOppgave1ReWrite\\TerjeOppgave1ReWrite\\myWalks – Kopi – Kopi.txt";
+            var WalkList = Walk.ReadWalksFromFile(filePath);
+
+            foreach (var Walk in WalkList)
+            {
+                Walk.printInfo(id);
+                id++;
+                totalDistance = totalDistance + Walk.CalculateDistance();
+            }
+            Console.WriteLine("*********************");
+            Console.WriteLine($"Nice Work! You've walked a total distance of {totalDistance} km");
+            Console.WriteLine("*********************");
+        }
+    }
+}
---
+++ TerjeOppgave1ReWrite/Walk.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TerjeOppgave1ReWrite
+{
+    public class Walk
+    {
+        public string Date { get; private set; }
+        public double Distance { get; private set; }
+
+        public Walk(string date, double distance)
+        {
+            Date = date;
+            Distance = distance;
+        }
+         
+        public static List<Walk> ReadWalksFromFile(string fileName)
+        {
+            var viewWalks = new List<Walk>();
+
+            foreach (string s in File.ReadLines(fileName))
+            {
+                string[] userInput = s.Split(',');
+                var stringDouble = userInput[1];
+                var kmWalked = Double.Parse(stringDouble, System.Globalization.CultureInfo.InvariantCulture);
+                var Walk = new Walk(userInput[1], kmWalked);
+                viewWalks.Add(Walk);
+            }
+            return viewWalks;
+        }
+        public void printInfo(int id)
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine($"Walk {id}");
+            Console.WriteLine($"Date: {Date}");
+            Console.WriteLine($"Distance {Distance} km");
+        }
+        public double CalculateDistance()
+        {
+            return Distance;
+        }
+    }
+}
